4ff264ae44d1759a47c0ca45f2d840ecd273a749
vikas rahangdale <vikasrahangdale5606@gmail.com>
2025-11-26 11:55:38 +0530

    doneee

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-     tailwindcss(),
-
+    tailwindcss(),
   ],
+  css: {
+    transformer: 'postcss'
+  }
 })
