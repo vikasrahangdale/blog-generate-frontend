145570508544a9fe79068d85eafcd1359655fa35
vikas rahangdale <vikasrahangdale5606@gmail.com>
2025-11-27 13:44:56 +0530

    doneeeeeeee vr

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true,
+  }
 });
